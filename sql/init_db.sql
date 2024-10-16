04dba23c3f6d4e9df3ecb089a233330d04590ee5
mvjdev <hei.maria.625@gmail.com>
2024-10-16 12:05:26 +0300

    16 octobre 2024-2025-àààà

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,10 +1,10 @@
-CREATE TABLE products_test (
+CREATE TABLE products_testsqq (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     price DECIMAL(10, 2)
 );
 
-INSERT INTO products_test (name, price) VALUES
+INSERT INTO products_testqqs (name, price) VALUES
 ('Product 1', 19.99),
 ('Product 2', 29.99);
 ('Product 3', 29.99);
