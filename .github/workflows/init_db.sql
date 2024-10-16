88759054ae2213948607f0820be3ccea12b204f8
mvjdev <hei.maria.625@gmail.com>
2024-10-16 08:58:24 +0300

    16 octobre 2024

---
+++ .github/workflows/init_db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE products_test (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    price DECIMAL(10, 2)
+);
+
+INSERT INTO products (name, price) VALUES
+('Product 1', 19.99),
+('Product 2', 29.99);
+('Product 3', 29.99);
